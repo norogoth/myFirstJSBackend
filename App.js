8c0142c262187392b57dd600d6b182e143947946
Landon Aaker <norogoth@gmail.com>
2021-03-26 15:31:31 -0700

    commit before I switch to MySQL

--- App.js
+++ App.js
@@ -1,13 +1,60 @@
-
 const express = require('express');
+const app = express();
 const sql = require('mssql');
+const http = require('http');
+const tedious = require('tedious');
+const hostname = '127.0.0.1';
+const port = 8000;
 
-async () => {
-    try {
-        await sql.connect('mssql://sa:44Grunkles^@localhost:1433');
-        const result = await sql.query`select * from test`
-        console.dir(result);
-    } catch (e) {
-        e.printStackTrace();        
+
+app.listen(port, hostname, () => {
+    console.log(`server is running on port ${port}`);
+})
+
+const data = null;
+const username = "sa";
+const password = "44Gorkles^";
+const connectionString = "mssql://"+username+":"+password+"@localhost:1433";
+
+var Connection = tedious.Connection;
+var config = {
+    server: 'localhost',
+    authentication: {
+        type: 'default',
+        options: {
+            userName: username,
+            password: password,
+        }
+    },
+    options: {
+        port: 1433,
+        //encrypt: false,
     }
-}
+};
+
+var conn = new Connection(config);
+conn.on('connect', function(err){
+    console.log('connected');
+});
+
+conn.connect();
+
+// (async () => {
+//     try {
+//         await sql.connect(connectionString);
+//         const result = await sql.query(`SELECT * FROM test`);
+//         const data = await result;
+//         console.dir(result);
+//     } catch (e) {
+//         console.log("error:", e.message);
+//     }
+// })();
+
+app.get('/test', (req, res) => {
+    return res.status(200).send({
+        success: 'true',
+        message: 'users',
+        data: data,
+    })
+})
+
