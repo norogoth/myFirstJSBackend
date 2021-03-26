b8d06a12edfe93cebd06d025eabb32a4a4e1bae6
Landon Aaker <norogoth@gmail.com>
2021-03-26 16:33:33 -0700

    Haven't gotten MySQL container working yet.

--- App.js
+++ App.js
@@ -16,39 +16,6 @@ const username = "sa";
 const password = "44Gorkles^";
 const connectionString = "mssql://"+username+":"+password+"@localhost:1433";
 
-var Connection = tedious.Connection;
-var config = {
-    server: 'localhost',
-    authentication: {
-        type: 'default',
-        options: {
-            userName: username,
-            password: password,
-        }
-    },
-    options: {
-        port: 1433,
-        //encrypt: false,
-    }
-};
-
-var conn = new Connection(config);
-conn.on('connect', function(err){
-    console.log('connected');
-});
-
-conn.connect();
-
-// (async () => {
-//     try {
-//         await sql.connect(connectionString);
-//         const result = await sql.query(`SELECT * FROM test`);
-//         const data = await result;
-//         console.dir(result);
-//     } catch (e) {
-//         console.log("error:", e.message);
-//     }
-// })();
 
 app.get('/test', (req, res) => {
     return res.status(200).send({
